330cdc449cd437af8309aab51a542cff0dfb2090
Ivan Roussev <ivan.roussev12@gmail.com>
2023-07-04 21:21:19 -0700

    added some changes

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import Navbar from './Navbar';
+import ivanPic from '../images/ivan1.jpg';
+
+function Home() {
+  return (
+    <div className='home-page'>
+      <Navbar />
+      <div className='main-home'>
+        <div className=''>
+          <img className='homepage-img' src={ivanPic} alt='ivan-pic-homepage' />
+        </div>
+        <div className='titles'>
+          <h1 className='title-name'>{'Ivan Roussev'}</h1>
+          <h1 className='software-engineer-title'>{'<Software Engineer />'}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className='menu-icon'>
+        <i className='fa fa-bars fa-2x'></i>
+      </div>
+      <div className='logo'>LOGO</div>
+      <div className='menu'>
+        <ul>
+          <li>
+            <a href='google.com'>Home</a>
+          </li>
+          <li>
+            <a href='google.com'>About</a>
+          </li>
+          <li>
+            <a href='google.com'>Projects</a>
+          </li>
+          <li>
+            <a href='google.com'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
