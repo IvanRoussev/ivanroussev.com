71c5a4044be9f8f8d1eb848aa00da20f76ddbec5
Ivan Roussev <ivan.roussev12@gmail.com>
2023-07-05 23:23:39 -0700

    adding some info in about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,21 @@ function About() {
   return (
     <div className='about' id='about'>
       <h1>About</h1>
+      <div className='about-content'>
+        <p>
+          I recently graduated from British Institute of Technology and am
+          excited to begin my career. Throughout my studies, I developed a
+          passion for programming and am constantly seeking new challenges to
+          enhance my skills. I thrive in collaborative environments where I can
+          work with other developers to solve complex problems and create
+          innovative solutions.I recently graduated from British Institute of
+          Technology and am excited to begin my career. Throughout my studies, I
+          developed a passion for programming and am constantly seeking new
+          challenges to enhance my skills. I thrive in collaborative
+          environments where I can work with other developers to solve complex
+          problems and create innovative solutions.
+        </p>
+      </div>
     </div>
   );
 }
