4e7772c9c8f3495e3bfc0dd1eeb7105aa8b209d7
Ivan Roussev <ivan@ivan-laptop.local>
2023-11-21 22:40:40 +0200

    added hamburger stack

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
+import hamburgerStack from '../images/icons8-hamburger-menu.svg';
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  };
+
   return (
     <nav>
       <div className='menu'>
         <ul>
           <div className='logo'>Mr. Roussev</div>
-          <div>
-            <li>
-              <a href='#home'>Home</a>
+          <div className='burger-menu'>
+            <li className='dvs-header__trigger' onClick={toggleMenu}>
+              <img src={hamburgerStack} alt='stack' />
             </li>
-            <li>
-              <a href='#about'>About</a>
-            </li>
-            <li>
-              <a href='#projects'>Projects</a>
-            </li>
-            <li>
-              <button className='contact-button'>
-                <a href='#contact'>Contact</a>
-              </button>
-            </li>
+            <div className={`dvs-header__menuItems ${isOpen ? 'is-open' : ''}`}>
+              <li>
+                <a href='#home'>Home</a>
+              </li>
+              <li>
+                <a href='#about'>About</a>
+              </li>
+              <li>
+                <a href='#projects'>Projects</a>
+              </li>
+              <li>
+                <button className='contact-button'>
+                  <a href='#contact'>Contact</a>
+                </button>
+              </li>
+            </div>
           </div>
         </ul>
       </div>
