6db3b858655b7bda36bf01c300e8303e17dce09d
Ivan Roussev <ivan@ivan-laptop.local>
2023-11-21 22:50:41 +0200

    vercel anyalutics udapte

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
+import Layout from './components/Layout.js';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
             <Contact />
           </div>
         </div>
+        <Layout />
       </div>
     </div>
   );
