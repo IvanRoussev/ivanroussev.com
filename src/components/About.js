441e394f9958cffae092090b5650fc7ff5b9512f
Ivan Roussev <ivan@ivan-laptop.local>
2023-11-22 20:06:05 +0200

    made more changes :)

--- src/components/About.js
+++ src/components/About.js
@@ -8,18 +8,13 @@ function About() {
         <h1 className='title'>About</h1>
         <div className='about-paragraph-container'>
           <p className='about-paragraph'>
-            I recently graduated from the British Columbia Institute of
-            Technology and am excited to begin my career. Throughout my studies,
-            I developed a passion for programming and am constantly seeking new
-            challenges to enhance my skills. I thrive in collaborative
-            environments where I can work with other developers to solve complex
-            problems and create innovative solutions. I recently graduated from
-            the British Institute of Technology and am excited to begin my
-            career. Throughout my studies, I developed a passion for programming
-            and am constantly seeking new challenges to enhance my skills. I
-            thrive in collaborative environments where I can work with other
-            developers to solve complex problems and create innovative
-            solutions.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div>
