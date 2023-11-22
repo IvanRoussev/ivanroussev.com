b7ad45f817a05f49a8823251d56a1630e086da7e
Ivan Roussev <ivan@ivan-laptop.local>
2023-11-22 18:05:59 +0200

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,39 @@
+import git from '../images/code-fork.png';
+
 function Projects() {
   return (
     <div className='projects' id='projects'>
       <div className='projects-content'>
         <h1 className='title'>Projects</h1>
+        <div className='project-container'>
+          <div className='project'>
+            <h3 className='project-headline'>Weather App</h3>
+            <a href='github'>
+              <img src={git} alt='git' className='project-git-link' />
+            </a>
+            <button className='view-project-button'>
+              <a>View Live</a>
+            </button>
+          </div>
+          <div className='project'>
+            <h3 className='project-headline'>Movies App</h3>
+            <a href='github'>
+              <img src={git} alt='git' className='project-git-link' />
+            </a>
+            <button className='view-project-button'>
+              <a>View Live</a>
+            </button>
+          </div>
+          <div className='project'>
+            <h3 className='project-headline'>Discord Bot</h3>
+            <a href='https://github.com/IvanRoussev?tab=repositories'>
+              <img src={git} alt='git' className='project-git-link' />
+            </a>
+            <button className='view-project-button'>
+              <a>View Live</a>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
